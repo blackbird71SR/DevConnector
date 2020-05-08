09bc80f2eed8f74a4a8f5e9efa26a6cd4bdcda0a
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-08 11:18:42 +0530

    Create base api routes

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
