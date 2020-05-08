bd4c1a0d57441966d61b8bb00752604fa864222f
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-08 21:14:52 +0530

    Return jsonwebtoken after registration It will be used by frontend for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const normalize = require("normalize-url");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -58,9 +60,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      //Return jsonwebtoken
 
-      res.send("User Registered");
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server Error!");
