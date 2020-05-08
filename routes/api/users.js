243d21ce7b24aa63950e499401150862deff415e
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-08 18:06:46 +0530

    Add check and validators to user registartion route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Include a valid email").isEmail(),
+    check("password", "Enter a Password with 6 or moe characters").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
