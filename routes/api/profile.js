7d686f475713c939202b7b616f052382fb0fb47c
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-09 22:08:13 +0530

    Add routes to get all profiles and profile based on user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -100,4 +100,40 @@ router.post(
   }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "No profile found." });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    // console.log(err.kind);
+    // if (err.kind == "ObjectId") {
+    //   return res.status(400).json({ msg: "No profile found." });
+    // }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
