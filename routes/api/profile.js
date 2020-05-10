39104f622f078b999eeb7a6ae1b0295ff0025969
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-10 06:51:28 +0530

    Delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -136,4 +136,20 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User removed!" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
