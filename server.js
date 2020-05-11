a4abfa383bbf72bfa715323eacae8abda0681331
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-11 20:35:42 +0530

    Fix a typo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/post", require("./routes/api/post"));
+app.use("/api/posts", require("./routes/api/post"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
