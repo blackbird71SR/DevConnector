608e1f0228f637195cab8a4ee91135256602ff31
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-16 10:04:30 +0530

    Remoev the previous code(We will use Redux)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from "react";
-import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -23,24 +22,7 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      const newUser = {
-        name,
-        email,
-        password,
-      };
-
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post("/api/users", body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log("Success");
     }
   };
 
