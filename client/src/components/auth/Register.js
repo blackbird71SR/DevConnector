8c1ec256570b9996a98491525c60c913ee64c1a4
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-16 10:02:59 +0530

    Access register route with axios within component(Temp)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -17,12 +18,29 @@ const Register = () => {
     });
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
     }
   };
 
