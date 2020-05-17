0b8f69d7cb1f50e4f2b7ef99996c05b870717eca
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-17 17:59:51 +0530

    Set loadUSer to run when page refresh in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -8,10 +8,19 @@ import Alert from "./components/layout/Alert";
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
-
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 import "./App.css";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
