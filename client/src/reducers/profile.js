1c259429810b54b69cb50d30e720cc3e6649be10
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-24 11:34:44 +0530

    Fix state

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -28,7 +28,7 @@ export default function (state = initialState, action) {
     case GET_PROFILES:
       return {
         ...state,
-        profile: payload,
+        profiles: payload,
         loading: false,
       };
     case PROFILE_ERROR:
