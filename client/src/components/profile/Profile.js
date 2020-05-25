7bd3852bb48d3827e6280c7015527c0d76f17620
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-25 10:23:47 +0530

    Get rid of warning

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -13,7 +13,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
