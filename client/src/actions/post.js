535c5ca5f530a571198de3c9849df88c63888d75
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-27 00:36:22 +0530

    Add delete post functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
 
 //Get posts
 export const getPosts = () => async (dispatch) => {
@@ -33,6 +33,20 @@ export const removeLike = (postId) => async (dispatch) => {
   try {
     const res = await axios.put(`/api/posts/unlike/${postId}`);
     dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Delete like
+export const deletePost = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`);
+    dispatch({ type: DELETE_POST, payload: postId });
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
