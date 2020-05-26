f2cb3358949539964ce2ee0b602ca4d423c04b83
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-27 01:45:13 +0530

    Add functionality to add comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from "./types";
 
 //Get posts
@@ -62,7 +64,7 @@ export const deletePost = (postId) => async (dispatch) => {
   }
 };
 
-//ADD like
+//ADD POST
 export const addPost = (formData) => async (dispatch) => {
   const config = {
     headers: {
@@ -86,6 +88,43 @@ export const getPost = (postId) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/posts/${postId}`);
     dispatch({ type: GET_POST, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//ADD Comment
+export const addComment = (postId, formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+    dispatch({ type: ADD_COMMENT, payload: res.data });
+    dispatch(setAlert("Comment Added", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Delete Comment
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({ type: REMOVE_COMMENT, payload: commentId });
+    dispatch(setAlert("Comment Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import Spinner from "../layout/Spinner";
 import { connect } from "react-redux";
 import { getPost } from "../../actions/post";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -18,6 +19,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
