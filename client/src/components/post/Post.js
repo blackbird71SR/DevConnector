0ad015710b23543b7189a970b35b2ca3b29acae3
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2020-05-27 02:08:54 +0530

    Add functionality to view comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { getPost } from "../../actions/post";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -20,6 +21,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
